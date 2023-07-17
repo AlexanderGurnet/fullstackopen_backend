327e5ac488b65f7f7c5495b09e5f9239f5045447
Alexander Gurnet <Alexandergurnet@gmail.com>
2023-07-17 11:38:50 +0300

    add info route

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+  response.send(`<div>
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date().toUTCString()}</p>
+  </div>`);
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
